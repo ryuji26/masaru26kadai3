2807fbe2904dfebdbba476ffdfbca23b44f049dc
ryuji <ryujispjp@gmail.com>
2021-06-04 21:24:00 +0900

    Initial Commit

---
+++ kadai3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kadai3
+//
+//  Created by 髙橋　竜治 on 2021/06/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

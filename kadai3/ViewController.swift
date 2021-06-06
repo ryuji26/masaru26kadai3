30fa828233f02948bd4a621e573a52cd0b314731
ryuji <ryujispjp@gmail.com>
2021-06-06 10:25:35 +0900

    no message

--- kadai3/ViewController.swift
+++ kadai3/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    @IBOutlet private weak var firstText: UITextField!
+    @IBOutlet private weak var secondText: UITextField!
+    @IBOutlet private weak var firstNumber: UILabel!
+    @IBOutlet private weak var secondNumber: UILabel!
+    @IBOutlet private weak var resultLabel: UILabel!
+    @IBOutlet private weak var firstSwitch: UISwitch!
+    @IBOutlet private weak var secondSwitch: UISwitch!
+
+    @IBAction func calculatedButton(_ sender: Any) {
+
+        //UITextFieldの型を変換
+        var firstTextNumber = Int(firstText.text ?? "") ?? 0
+        var secondTextNumber = Int(secondText.text ?? "") ?? 0
+
+        let resultText: String
+
+        //UISwitchによる符号の切り替え
+        if firstSwitch.isOn {
+            firstTextNumber *= -1
+        }
+
+        if secondSwitch.isOn {
+            secondTextNumber *= -1
+        }
+
+        //計算して各UILabelに表示
+        firstNumber.text = String(firstTextNumber)
+
+        secondNumber.text = String(secondTextNumber)
+
+        resultText = String(firstTextNumber + secondTextNumber)
+        resultLabel.text = resultText
+    }
+    
 
 }
 

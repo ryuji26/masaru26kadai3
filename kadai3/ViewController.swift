288090e80749088af54d3cead80a60dd9a94a719
akio0911 <akio0911@gmail.com>
2021-06-07 12:19:16 +0900

    feedback

--- kadai3/ViewController.swift
+++ kadai3/ViewController.swift
@@ -18,23 +18,20 @@ class ViewController: UIViewController {
     @IBOutlet private weak var secondSwitch: UISwitch!
 
     @IBAction func calculatedButton(_ sender: Any) {
-
         // UITextFieldの型を変換
         let firstTextNumber = Int(firstText.text ?? "") ?? 0
         let secondTextNumber = Int(secondText.text ?? "") ?? 0
 
-        let firstSwitchNumber: Int
-        let secondSwitchNumber: Int
-
-        let resultText: String
-
         // UISwitchによる符号の切り替え
+
+        let firstSwitchNumber: Int
         if firstSwitch.isOn {
             firstSwitchNumber = firstTextNumber * -1
         } else {
             firstSwitchNumber = firstTextNumber
         }
 
+        let secondSwitchNumber: Int
         if secondSwitch.isOn {
             secondSwitchNumber = secondTextNumber * -1
         } else {
@@ -43,10 +40,7 @@ class ViewController: UIViewController {
 
         // 計算して各UILabelに表示
         firstNumber.text = String(firstSwitchNumber)
-
         secondNumber.text = String(secondSwitchNumber)
-
-        resultText = String(firstSwitchNumber + secondSwitchNumber)
-        resultLabel.text = resultText
+        resultLabel.text = String(firstSwitchNumber + secondSwitchNumber)
     }
 }
